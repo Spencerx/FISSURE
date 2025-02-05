3b7ad2e545a29af2d5c11e446188baa6cc052e27
joereith <reithj@ainfosec.com>
2025-02-05 15:13:11 -0500

    Update library.py
    changing default port of postgresql to not interfere with TakServer

--- fissure/utils/library.py
+++ fissure/utils/library.py
@@ -52,7 +52,7 @@ def openDatabaseConnection(retries=10, delay=2) -> connection:
     user = os.getenv('POSTGRES_USER')
     password = os.getenv('POSTGRES_PASSWORD')
     host = os.getenv('POSTGRES_HOST', 'localhost')
-    port = os.getenv('POSTGRES_PORT', '5432')
+    port = os.getenv('POSTGRES_PORT', '5431')
 
     for attempt in range(retries):
         try:
